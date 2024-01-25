72f2343c46db4115d98549c96a14fd41246083e8
SirDalbag <sir.dalbag@gmail.com>
2024-01-25 19:01:17 +0600

    update

--- leetcode/main.py
+++ leetcode/main.py
@@ -11,3 +11,15 @@ def searchInsert(nums: list[int], target: int) -> int:
 def lengthOfLastWord(s: str) -> int:
     s = s.split()
     return len(s[len(s) - 1])
+
+
+# TODO Given an integer, convert it to a roman numeral.
+def intToRoman(num: int) -> str:
+    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    result = ""
+    for i, value in enumerate(values):
+        while num >= value:
+            num -= value
+            result += symbols[i]
+    return result
