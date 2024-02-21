f69822d9582ba0eaaf1bc1a03bb99eab07fcba92
SirDalbag <sir.dalbag@gmail.com>
2024-02-21 12:36:54 +0600

    update

--- django_fullstack/frontend/src/pages/search.tsx
+++ django_fullstack/frontend/src/pages/search.tsx
@@ -4,6 +4,7 @@ import * as bases from "../components/bases";
 
 export default function Page() {
   const [searchValue, setSearchValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearchSubmit = async (e: any) => {
     e.preventDefault();
@@ -11,7 +12,7 @@ export default function Page() {
     try {
       const url = "http://127.0.0.1:8000/api/";
       const response = await axios.post(url, { search: searchValue });
-      console.log(response.data);
+      setSearchResults(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -19,7 +20,7 @@ export default function Page() {
 
   return (
     <bases.Base>
-      <div className="flex justify-center p-4">
+      <div className="flex flex-col gap-3 items-center p-4">
         <form onSubmit={handleSearchSubmit}>
           <div className="relative mt-2 rounded-md shadow-sm">
             <input
@@ -28,9 +29,14 @@ export default function Page() {
               id="search"
               className="block w-[500px] rounded-md border-0 py-4 pl-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600"
               placeholder="Книги, авторы, жанры"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
             />
           </div>
         </form>
+        <div>
+          <h1>{(searchResults as any).message}</h1>
+        </div>
       </div>
     </bases.Base>
   );
